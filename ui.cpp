561bb9270b3462695b7b2b264c0993f2ab798682
Markus Wick <markus@selfnet.de>
2010-11-30 21:54:27 +0100

    ui

--- ui.cpp
+++ ui.cpp
@@ -8,23 +8,28 @@ UInterface::UInterface()
 
 void UInterface::init()
 {
-	initGL();
+	// Slightly different SDL initialization
+	if ( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0 ) {
+		printf("Unable to initialize SDL: %s\n", SDL_GetError());
+	}
+	
+	initWindow();
+	
+	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+
+	SDL_ShowCursor(SDL_DISABLE);
 }
 
 void UInterface::config(const boost::program_options::variables_map &c)
 {
 	noFullX 			= c["noFullX"].as<int>();
 	noFullY 			= c["noFullY"].as<int>();
-	isFullscreen 	= c["fullscreen"].as<bool>();
+	isFullscreen 		= c["fullscreen"].as<bool>();
 }
 
 
-void UInterface::initGL() {
-	// Slightly different SDL initialization
-	if ( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0 ) {
-		printf("Unable to initialize SDL: %s\n", SDL_GetError());
-	}
-
+void UInterface::initWindow()
+{
 	if (isFullscreen) {
 		const SDL_VideoInfo *vi = SDL_GetVideoInfo();
 		screenX = vi->current_w;
@@ -48,20 +53,73 @@ void UInterface::initGL() {
 	gluPerspective(45.0f, (GLfloat) screenX / (GLfloat) screenY, 0.01f, 1000.0f);
 	glMatrixMode(GL_MODELVIEW);	// Select The Modelview Matrix
 	glLoadIdentity();					// Reset The Projection Matrix
-	
-	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
-
-	SDL_ShowCursor(SDL_DISABLE);
-
- /* //generate Textures
-  glGenTextures( numberOfTex, texture );
-
-  for(int i = 1; i < numberOfTex; i++){
-    loadTexture(texFiles[i], i);
-  }
-
-*/
-
-//	SDL_TimerID timer = SDL_AddTimer(40,GameLoopTimer,0);
 
 }
+
+
+void UInterface::run()
+{
+	SDL_Event event;
+
+	while((!done) && (SDL_WaitEvent(&event))) {
+		switch(event.type) {
+			case SDL_USEREVENT:			handleUserEvents(event.user);			break;
+			case SDL_KEYDOWN:			handleKeyDownEvents(event.key); 		break;
+			case SDL_KEYUP:				handleKeyUpEvents(event.key); 			break;
+			case SDL_MOUSEBUTTONDOWN: 	handleMouseDownEvents(event.button); 	break;
+			case SDL_MOUSEBUTTONUP: 	handleMouseUPEvents(event.button); 		break;
+			case SDL_MOUSEMOTION: 		handleMouseEvents(event.motion); 		break;
+			case SDL_QUIT: 				done = true;  							break;
+
+			case SDL_VIDEORESIZE:
+				screenX = event.resize.w;
+				screenY = event.resize.h;
+				initWindow();
+				break;
+		}
+	}
+}
+
+void UInterface::handleKeyDownEvents(SDL_KeyboardEvent e)
+{
+	switch(e.keysym.sym){
+		
+	}
+}
+
+void UInterface::handleKeyUpEvents(SDL_KeyboardEvent e)
+{
+	switch(e.keysym.sym){
+		
+	}
+}
+
+void UInterface::handleUserEvents(SDL_UserEvent e)
+{
+	
+//	debug();
+
+}
+
+void UInterface::handleMouseDownEvents(SDL_MouseButtonEvent e)
+{
+
+}
+
+void UInterface::handleMouseUPEvents(SDL_MouseButtonEvent e)
+{
+
+}
+
+void UInterface::handleMouseEvents(SDL_MouseMotionEvent e)
+{
+	if(catchMouse) {
+		int x = e.x-screenX/2;
+		int y = e.y-screenY/2;
+		
+		SDL_WarpMouse(screenX/2, screenY/2);
+	}
+}
+
+
+
--- ui.h
+++ ui.h
@@ -25,9 +25,19 @@ public:
 	void init();
 
 	void config(const boost::program_options::variables_map &c);
+	
+	void run();
 
 private:
-	void initGL();
+	void initWindow();
+	
+	void handleUserEvents(SDL_UserEvent e);
+	void handleKeyDownEvents(SDL_KeyboardEvent e);
+	void handleKeyUpEvents(SDL_KeyboardEvent e);
+	void handleMouseDownEvents(SDL_MouseButtonEvent e);
+	void handleMouseUPEvents(SDL_MouseButtonEvent e);
+	void handleMouseEvents(SDL_MouseMotionEvent e);
+	
 
 	//Frame conditions
 	//current frame size
