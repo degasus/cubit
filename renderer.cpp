ea078e63b97bb5c113f23390ffd7727d4c713901
Markus Wick <markus@selfnet.de>
2011-01-05 03:19:03 +0100

    sqrt(3)

--- renderer.cpp
+++ renderer.cpp
@@ -290,10 +290,10 @@ bool Renderer::areaInViewport(BlockPosition apos, PlayerPosition ppos) {
 	std::cout << apos.z << " " << ppos.z << std::endl;
 	std::cout << std::endl;
 	*/
-	return (erg.data[0][0] > - AREASIZE_X/2) 				// nicht hinter einem
+	return (erg.data[0][0] > - AREASIZE_X/1.7321) 				// nicht hinter einem
 	    && (erg.data[0][0] < AREASIZE_X*visualRange)	// sichtweite
-		 && (abs(erg.data[0][1])/(abs(erg.data[0][0])+AREASIZE_X*2) < (double(c->ui.screenX) / c->ui.screenY)/2 )	// seitlich ausm sichtbereich
-		 && (abs(erg.data[0][2])/(abs(erg.data[0][0])+AREASIZE_X*2) < 0.5 )	// seitlich ausm sichtbereich
+		 && (abs(erg.data[0][1])/(abs(erg.data[0][0])+AREASIZE_X*1.7321) < (double(c->ui.screenX) / c->ui.screenY)/2 )	// seitlich ausm sichtbereich
+		 && (abs(erg.data[0][2])/(abs(erg.data[0][0])+AREASIZE_X*1.7321) < 0.5 )	// seitlich ausm sichtbereich
 		 ;
 }
 void Renderer::render(PlayerPosition pos)
